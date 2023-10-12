87e4f57a71322ef60e4c3b1206f6119e7cc2f790
Kaleb Weaver <kcweaver2@bsu.edu>
2023-10-12 05:47:19 -0400

    Add working 3d gun launcher

--- launcher/launcher.gd
+++ launcher/launcher.gd
@@ -1,26 +1,25 @@
 extends Node3D
 
-var bird : bird
 
-@export var force := Vector3(-2,0,0)
+
+@export var force := Vector3(-1000,-10,-1000)
+@onready var bird = preload("res://birds/bird.tscn")
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	
-	_make_new_bird()
+#func _ready():
+#	_make_new_bird()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta:float):
+func _process(delta:float):
 	if Input.is_action_just_pressed("launch"):
-		
-		bird.apply_impulse(force)
 		_make_new_bird()
-	
+
 
 
 		
 func _make_new_bird():
-	
-	bird = preload("res://birds/bird.tscn").instantiate()
-	call_deferred("add_child", bird)
+	var bird_inst = bird.instantiate()
+	get_tree().get_root().get_child(0).add_child(bird_inst)
+	bird_inst.global_transform = $launch_point_3d.global_transform
+	bird_inst.apply_central_impulse(force*-self.global_transform.basis.z)
